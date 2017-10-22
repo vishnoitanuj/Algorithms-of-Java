5784f0bb08a0d379b90c424b2a49c5fda89f3f9c
TANUJ VISHNOI <vishnoitanuj@outlook.com>
2017-10-22 19:10:59 +0530

    Till Insertion Sort

---
+++ Sorting/insertionSort.java
@@ -0,0 +1,36 @@
+/*This program deals with Insertion Sort Algorithm. In this the array gets sorted from the left side.
+Time Complexity -: 
+Best Case:		O(n)
+Average Case:	o(n^2) 
+Worst Case:		O(n^2) */
+import java.util.Scanner;
+class insertionSort{
+	public static void sort(int[] a){
+		int i,gap,value,temp;
+		for(i=1;i<a.length;i++){
+			value=a[i];
+			gap=i;
+			while(gap>0 && a[gap-1]>value){
+				a[gap]=a[gap-1];
+				gap--;
+			}
+			a[gap]=value;
+		}
+		
+		System.out.println("-----------------------------------");
+		System.out.println("Sorted Array:");
+		for(i=0;i<a.length;i++)
+			System.out.print(a[i]+"\t");
+	}
+	public static void main(String args[])throws Exception{
+		Scanner d=new Scanner(System.in);
+		System.out.println("Enter length of array");
+		int n=d.nextInt();
+		int[] a =new int[n];
+		System.out.println("Enter elements of array");
+		for(int i=0;i<n;i++){
+			a[i]=d.nextInt();
+		}
+		sort(a);
+	}
+}
---
+++ Sorting/selectionSort.java
@@ -0,0 +1,33 @@
+/*This program deals with selection sort Algorithm. In this the min/max element is selected and moved to its right index.
+Time Complexity:  O(n^2) */
+import java.util.Scanner;
+class selectionSort{
+	public static void sort(int[] a){
+		int i,j,min,temp;
+		for(i=0;i<a.length-1;i++){
+			min=a[i];
+			for(j=i+1;j<a.length;j++){
+				if(a[j]<min)
+					min=j;
+			}
+			temp=a[i];
+			a[i]=a[min];
+			a[min]=temp;
+		}
+		System.out.println("-----------------------------------");
+		System.out.println("Sorted Array:");
+		for(i=0;i<a.length;i++)
+			System.out.print(a[i]+"\t");
+	}
+	public static void main(String args[])throws Exception{
+		Scanner d=new Scanner(System.in);
+		System.out.println("Enter length of array");
+		int n=d.nextInt();
+		int[] a =new int[n];
+		System.out.println("Enter elements of array");
+		for(int i=0;i<n;i++){
+			a[i]=d.nextInt();
+		}
+		sort(a);
+	}
+}
